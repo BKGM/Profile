08f226ff6d27b5469da9e1533080486a2a95341c
Nguyen Tuan Anh <anhnt.fami@gmail.com>
2015-03-17 11:28:53 +0700

    f

---
+++ app.js
@@ -0,0 +1,225 @@
+SIGN_products = [{
+    code: "pianoic",
+    name: "Pianoic",
+    img: "pianoic.jpg",
+    links: [{
+        name: "website",
+        href: "http://pianoic.com/"
+    }]
+}, {
+    code: "slaprat",
+    name: "Slap Rat",
+    img: "slaprat.jpg",
+    links: [{
+        name: "facebook",
+        href: "https://apps.facebook.com/slaprat/"
+    }, {
+        name: "iOS",
+        href: "https://itunes.apple.com/us/app/slap-rat/id831225475?mt=8"
+    }, {
+        name: "android",
+        href: "https://play.google.com/store/apps/details?id=com.BKGameMaker.SlapRat"
+    }]
+}, {
+    code: "amduonglich",
+    name: "Am duong lich",
+    img: "amduonglich.png",
+    links: [{
+        name: "website",
+        href: "http://amduonglich.com/"
+    }]
+}, {
+    code: "seen",
+    name: "SEEN",
+    img: "seen.jpg",
+    links: [{
+        name: "facebook",
+        href: "http://apps.facebook.com/seen_bkgm"
+    }, {
+        name: "iOS",
+        coming: !0
+    }, {
+        name: "android",
+        coming: !0
+    }]
+}, {
+    code: "watdanum",
+    name: "WAT DA' NUM",
+    img: "watdanum.jpg",
+    sold: !0
+}, {
+    code: "clgt",
+    name: "Co Loa Guard Tower",
+    img: "clgt.jpg",
+    sold: !0
+}, {
+    code: "evenodd",
+    name: "Even Odd",
+    img: "evenodd.jpg",
+    sold: !0
+}, {
+    code: "whitedrop",
+    name: "White Drop",
+    img: "whitedrop.png",
+    links: [{
+        name: "facebook",
+        href: "https://apps.facebook.com/whitedrop/"
+    }, {
+        name: "iOS",
+        href: "https://itunes.apple.com/us/app/white-drop/id838094757?mt=8"
+    }, {
+        name: "android",
+        href: "https://play.google.com/store/apps/details?id=com.BKGM.WhiteDrop"
+    }]
+}], SIGN_members = [{
+    name: "Dinh Hoang Anh",
+    position: "Mobile Developer"
+}, {
+    name: "Nguyen Van Trung",
+    position: "Backend Developer"
+}, {
+    name: "Nguyen Tuan Anh",
+    position: "Frontend Developer"
+}], SIGN_EN = {
+    p0l2: "multiplatform",
+    p0r2: "web, games and applications",
+    p0button: "our products",
+    "scroll down": "scroll down",
+    p1d1: "we develop a wide range of creative games",
+    p1d2: "on multiple platforms, from web browser to mobile devices",
+    p2d1: "we develop multiplatform applications",
+    p2d2: "that runs smoothly with stunning interface on every devices",
+    p3d1: "we build websites and web services",
+    p3d2: "with elegant and modern designs on latest technologies",
+    bookdesc: "BKGM's Collection",
+    bookdesc1: "BKGM creates",
+    bookdesc2: "the differences",
+    BKGMTeam: "BKGM team",
+    BKGMDesc: "we are fearless assholes aiming for the art of technology.",
+    home: "home",
+    product: "products"
+}, SIGN_VI = {
+    p0l2: "web và game",
+    p0r2: "ứng dụng đa nền tảng",
+    p0button: "xem sản phẩm",
+    "scroll down": "xem tiếp",
+    p1d1: "chúng tôi phát triển nhiều thể loại game sáng tạo",
+    p1d2: "trên hầu hết mọi nền tảng, từ trình duyệt đến thiết bị di động",
+    p2d1: "chúng tôi phát triển những ứng dụng",
+    p2d2: "hoạt động mượt mà với giao diện hấp dẫn trên mọi thiết bị",
+    p3d1: "chúng tôi xây dựng những website",
+    p3d2: "có thiết kế tinh tế và hiện đại với những công nghệ mới nhất",
+    bookdesc: "Bộ sưu tập những sản phẩm nổi bật của BKGM",
+    bookdesc1: "BKGM tạo ra",
+    bookdesc2: "sự khác biệt",
+    BKGMTeam: "nhóm BKGM",
+    BKGMDesc: "Chúng tôi là những kẻ liều lĩnh đam mê công nghệ và sự hoa mỹ.",
+    home: "trang chủ",
+    product: "sản phẩm"
+},
+function() {
+    var e = (navigator.userAgent, function() {
+        var e = !1;
+        return function(n) {
+            (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(n) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(n.substr(0, 4))) && (e = !0)
+        }(navigator.userAgent || navigator.vendor || window.opera), e
+    }());
+    $(function() {
+        e ? (smoothScroll.init({
+            speed: 100
+        }), $(function() {
+            FastClick.attach(document.body)
+        })) : (smoothScroll.init({
+            speed: 500
+        }), $.stellar({
+            horizontalScrolling: !1,
+            responsive: !0
+        }))
+    }), angular.module("SIGNBook", ["ngRoute"]), angular.module("SIGNBook").config(["$routeProvider", "$locationProvider",
+        function(e, n) {
+            n.hashPrefix("!"), e.when("/lang/:langId", {
+                templateUrl: "non.html"
+            }), e.otherwise({
+                redirectTo: "/lang/en"
+            })
+        }
+    ]),
+    function() {
+        var n = [{
+                name: "en",
+                src: SIGN_EN
+            }, {
+                name: "vi",
+                src: SIGN_VI
+            }],
+            o = !0;
+        angular.module("SIGNBook").controller("SIGNCtrl", ["$scope", "$location", "$timeout",
+            function(a, t, i) {
+                a.isMobile = e;
+                var r = (a.products = SIGN_products, a.members = SIGN_members, a.s = {
+                    language: 1
+                });
+                a.languages = n, a.updateLanguage = function(e) {
+                    a.L = e.src, a.Lname = e.name, t.path("/lang/" + a.Lname)
+                }, a.$watch("s.language", function() {
+                    r.language < n.length && i(function() {
+                        a.updateLanguage(n[r.language])
+                    }, 0), o && (i(function() {
+                        $(".loader-wrapper").fadeOut("slow", function() {
+                            $(this).remove()
+                        })
+                    }, 0), o = !1)
+                })
+            }
+        ])
+    }()
+}(), angular.module("SIGNBook").factory("Page", function() {
+    var e = "http://signworld.vn/feature_img.jpg",
+        n = "";
+    return {
+        feature_img: function() {
+            return e
+        },
+        set_feature_img: function(n) {
+            e = n
+        },
+        feature_title: function() {
+            return n
+        },
+        set_feature_title: function(e) {
+            n = e
+        },
+        reset: function() {
+            e = "http://signworld.vn/feature_img.jpg", n = ""
+        }
+    }
+}), angular.module("SIGNBook").factory("languages", function() {
+    return {
+        list: [{
+            name: "en",
+            src: SIGN_EN
+        }, {
+            name: "vi",
+            src: SIGN_VI
+        }],
+        codes: {
+            en: 0,
+            vi: 1,
+            fr: 2,
+            jp: 3,
+            kr: 4
+        }
+    }
+}),
+function() {
+    var e = {
+        en: 0,
+        vi: 1
+    };
+    angular.module("SIGNBook").controller("NonCtrl", ["$scope", "$routeParams", "$location",
+        function(e, t, o) {
+            var a = e.s;
+            console.log(t.langId), a.language = n[t.langId], o.path("/lang/" + t.langId)
+        }
+    ])
+}();
